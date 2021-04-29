e745fd8c3db1956460307e0eacf68647d9de67ff
vikramsvdd <51330665+vikramsvdd@users.noreply.github.com>
2021-04-29 10:30:36 +0530

    Update Activity3.h

--- inc/Activity3.h
+++ inc/Activity3.h
@@ -1,11 +1,11 @@
 #ifndef _ACTIVITY3_h_
 #define _ACTIVITY3_h_
 /**
- * @file activity1.h
- * @author Rahul S Iyer
+ * @file Activity3.h
+ * @author Vikram Vasudev
  * @brief 
  * @version 0.1
- * @date 2021-04-23
+ * @date 2021-04-25
  * 
  * @copyright Copyright (c) 2021
  * 
