c347770b9c89e23f4fd92144d8fe206c1af39a80
vikramsvdd <51330665+vikramsvdd@users.noreply.github.com>
2021-04-29 11:53:35 +0530

    Update a.c

--- src/a.c
+++ src/a.c
@@ -8,7 +8,6 @@ void set_PWM(void)
     DDRB |= (1<<PWM_PORT);           // To set B1 port as PWM Output for Timer1
     TCCR1A |= (1<<COM1A1) | (1<<WGM11) | (1<<WGM10);     
     TCCR1B |= (1<<WGM12)| (1<<CS11) | (1<<CS10);
-     UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 }
 
 void UART_init()
@@ -16,7 +15,9 @@ void UART_init()
     value=BAUD_RATE;
     UBRR0H =(value>>8)&0x0ff;                         //setting Baud rate
     UBRR0L = value;
-    UCSR0C=(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);    //Enable reciever and transmitter
+    UCSR0C=(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);    
+    //Enable reciever and transmitter
+    UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 
                                                                                 
 }
