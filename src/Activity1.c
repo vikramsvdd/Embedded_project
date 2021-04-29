feeb99abb0e36bf5a5c6175909c5464af3c57e13
vikramsvdd <51330665+vikramsvdd@users.noreply.github.com>
2021-04-29 10:38:02 +0530

    Create Activity1.c

---
+++ src/Activity1.c
@@ -0,0 +1,21 @@
+#include "Activity1.h"
+#include "Activity2.h"
+#include "Activity3.h"             // including all the header files within the same source directory
+#include "Activity4.h"
+
+void LED_ON()
+{
+	LED_PORT |= (1<<LED_PIN);
+}
+void LED_OFF()
+{
+	LED_PORT &= ~(1<<LED_PIN);
+}
+void Peripheral_io(void)
+{
+  DDRD |= (1<<LED_PIN);                      // to set D port as output for led
+  DDRC &= ~(1<<HEATER_PIN);                  // to set C port as the input for heater button
+  DDRB &= ~(1<<BUTTON_PIN);                   //to set B port as input to check whether a passenger is present or not
+  HEATER_PORT |= (1<<HEATER_PIN);
+  BUTTON_PORT |= (1<<BUTTON_PIN);
+}
