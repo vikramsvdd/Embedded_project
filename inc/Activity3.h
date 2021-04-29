48d99bfa0fbc16abd1258e60dd88d3e6ff97d386
vikramsvdd <51330665+vikramsvdd@users.noreply.github.com>
2021-04-29 10:24:00 +0530

    Create Activity3.h

---
+++ inc/Activity3.h
@@ -0,0 +1,19 @@
+#ifndef _ACTIVITY3_h_
+#define _ACTIVITY3_h_
+/**
+ * @file activity1.h
+ * @author Rahul S Iyer
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * @brief Function to set the PWM output
+ * 
+ */
+void set_PWM();
+#endif
